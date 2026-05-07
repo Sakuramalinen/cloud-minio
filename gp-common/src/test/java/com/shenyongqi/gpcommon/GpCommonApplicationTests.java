996dcf457285ae4d9f86805842befb86ab0a069b
shenyongqi <3186200020@qq.com>
2026-05-07 17:30:54 +0800

    初始工程

---
+++ gp-common/src/test/java/com/shenyongqi/gpcommon/GpCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenyongqi.gpcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gp-gateway/src/test/java/com/shenyongqi/gpgateway/GpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenyongqi.gpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
